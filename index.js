985e0e4e8b1bbece08f8ebe1248cbd559de3b84d
olof-sky <oloof92@gmail.com>
2022-08-16 12:58:09 +0200

    Refactor

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const readLine = require('readline');
 const fs = require('fs');
-var file = './samples/002.in';
+var file = './samples/003.in';
 
 // initialAnnoyance, annoyanceMultiplicator, annoyanceLevel, askedStatus(Boolean), index
 let coworkers = []
 
 // First time iteration setter
 let first = true;
-
 let leastAnnoyedCoworker;
 let mostAnnoyedCoworker;
 
@@ -18,55 +17,99 @@ var rl = readLine.createInterface({
 });
 
 function setUpCoworkersIndex() {
-  for (let i = 1; i < parseInt(coworkers[0][1]) + 1; i++) {
-    coworkers[i][4] = i;
-  }
-}
+  for (let i = 1; i < parseInt(coworkers[0][1]) + 1; i++) { coworkers[i][4] = i; }
+} 
 
 function getMostAnnoyed() {
   for (let i = 1; i < parseInt(coworkers[0][1]) + 1; i++) {
-    if (coworkers[i][0] > mostAnnoyedCoworker[2] || coworkers[i][2] > mostAnnoyedCoworker[2]) {
-      mostAnnoyedCoworker = coworkers[i]
+    const coworkerTotal = parseInt(coworkers[i][2])
+    const mostAnnoyedCoworkerTotal = parseInt(mostAnnoyedCoworker[2])
+    if (coworkers[i][3] === "false") {
+      coworkers[i][2] = parseInt(coworkers[i][0])
+    }
+    if (mostAnnoyedCoworker[2] === "false") {
+      mostAnnoyedCoworker[2] = parseInt(mostAnnoyedCoworker[0])
+    }
+    if (coworkerTotal > mostAnnoyedCoworkerTotal) {
+        mostAnnoyedCoworker = coworkers[i] 
+    }
+  }
+}
+
+function setLeastAnnoyedCoworker(coworker, isAsked) {
+  const leastAnnoyedTotalPlusIncrement = parseInt(leastAnnoyedCoworker[2]) + parseInt(leastAnnoyedCoworker[1])
+  const leastAnnoyedInitialPlusIncrement = parseInt(leastAnnoyedCoworker[0]) + parseInt(leastAnnoyedCoworker[1])
+
+  if (isAsked) {
+    const coworkerTotalPlusIncrement = parseInt(coworker[2]) + parseInt(coworker[1])
+    if (leastAnnoyedCoworker[3] === true) {
+      if (coworkerTotalPlusIncrement > leastAnnoyedTotalPlusIncrement) { return; }
+      else leastAnnoyedCoworker = coworker;
+    }
+    if (leastAnnoyedCoworker[3] === "false") {
+      if (coworkerTotalPlusIncrement > leastAnnoyedInitialPlusIncrement) { return; }
+      else leastAnnoyedCoworker = coworker;
+    }
+  }
+
+  else {
+    const coworkerInitialPlusIncrement = parseInt(coworker[0]) + parseInt(coworker[1])
+    if (leastAnnoyedCoworker[3] === true) {
+      if (coworkerInitialPlusIncrement > leastAnnoyedTotalPlusIncrement) { return; }
+      else leastAnnoyedCoworker = coworker;
+    }
+    if (leastAnnoyedCoworker[3] === "false") {
+      if (coworkerInitialPlusIncrement > leastAnnoyedInitialPlusIncrement) { return; }
+      else leastAnnoyedCoworker = coworker;
     }
   }
 }
 
 function getCoworkerAnnoyance() {
   for (let i = 1; i < parseInt(coworkers[0][1]) + 1; i++) {
-    // If total is less then current leastAnnoyed and coworker has been asked before.
-    if (coworkers[i][2] <= leastAnnoyedCoworker[2] && coworkers[i][3] === true ) {
-      console.log("If true")
-      // Check for each coworker
+    const coworkerTotalPlusIncrement = parseInt(coworkers[i][2]) + parseInt(coworkers[i][1])
+    const coworkerInitialPlusIncrement = parseInt(coworkers[i][0]) + parseInt(coworkers[i][1])
+    if (coworkers[i][3] === true ) {
       for (let j = 1; j < (parseInt(coworkers[0][1]) + 1); j++) {
-        console.log("Iterate through " + coworkers[j])
-        // If increment is higher or equal to the next coworkers initial annoyance
-        if (coworkers[i][1] >= coworkers[j][1] && coworkers[j][3] === true) {
-          console.log("Is increase higher then initial and new is true")
-          if (coworkers[i][2] > coworkers[j][2]) {
-            leastAnnoyedCoworker = coworkers[j]
-          }
+        if (coworkers[j][3] === true ) {
+          const nextCoworkerTotalPlusIncrement = parseInt(coworkers[j][2]) + parseInt(coworkers[j][1])
+          if (coworkerTotalPlusIncrement > nextCoworkerTotalPlusIncrement) { setLeastAnnoyedCoworker(coworkers[j], true) }
+          else setLeastAnnoyedCoworker(coworkers[i], true)
+        }
+        if (coworkers[j][3] === "false" ) {
+          const nextCoworkerInitialPlusIncrement = parseInt(coworkers[j][0]) + parseInt(coworkers[j][1])
+          if (coworkerTotalPlusIncrement > nextCoworkerInitialPlusIncrement) { setLeastAnnoyedCoworker(coworkers[j], false) }
+          else setLeastAnnoyedCoworker(coworkers[i], true)
         }
       }
     }
-    // If first time, set leastAnnoyed to lowest initial annoyance.
-    if (coworkers[i][0] <= leastAnnoyedCoworker[2] && coworkers[i][3] === "false") { leastAnnoyedCoworker = coworkers[i] }
+
+    if (coworkers[i][3] === "false" ) {
+      for (let j = 1; j < (parseInt(coworkers[0][1]) + 1); j++) {
+        if (coworkers[j][3] === true ) {
+          const nextCoworkerTotalPlusIncrement = parseInt(coworkers[j][2]) + parseInt(coworkers[j][1])
+          if (coworkerInitialPlusIncrement > nextCoworkerTotalPlusIncrement) { setLeastAnnoyedCoworker(coworkers[j], true) }
+          else setLeastAnnoyedCoworker(coworkers[i], false)
+        }
+        if (coworkers[j][3] === "false" ) {
+          const nextCoworkerInitialPlusIncrement = parseInt(coworkers[j][0]) + parseInt(coworkers[j][1])
+          if (coworkerInitialPlusIncrement > nextCoworkerInitialPlusIncrement) { setLeastAnnoyedCoworker(coworkers[j], false) }
+          else setLeastAnnoyedCoworker(coworkers[i], false)
+        }
+      }
+    }
   }
 }
 
 function askCoworker(coworker) {
   // If asked first time
-  console.log("ASK COWORKER ", coworkers[coworker[4]])
   if (coworkers[coworker[4]][3] === "false") {
     coworkers[coworker[4]][2] = (parseInt(coworker[0]) + parseInt(coworker[1]))
-    console.log(coworkers[coworker[4]][2] + " FIRST")
   }
-  
   // If asked before
   if (coworkers[coworker[4]][3] === true) { 
     coworkers[coworker[4]][2] += parseInt(coworker[1])
-    console.log(coworkers[coworker[4]][2] + " SECOND")
   }
-
   coworkers[coworker[4]][3] = true;
 }
 
@@ -84,10 +127,8 @@ rl.on('close', function () {
       first = false;
     }
     getCoworkerAnnoyance();
-    console.log("LEAST ANNOYED", leastAnnoyedCoworker)
     askCoworker(leastAnnoyedCoworker);
-    getMostAnnoyed();
   }
-  console.log("Least annoyed", leastAnnoyedCoworker[2]);
-  console.log("Most annoyed", mostAnnoyedCoworker[2]);
+  getMostAnnoyed();
+  console.log(mostAnnoyedCoworker[2]);
 });
